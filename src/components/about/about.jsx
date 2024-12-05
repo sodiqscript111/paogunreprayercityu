08b77d00b0d2f5f17237deb075a41a52decb6fd3
simidusodiqt <simidusodiqt@gmail.com>
2024-12-05 13:14:31 +0000

    few changes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
     <div className="about-us">
       <div className="about-us-content">
         <h2>About Us</h2>
-        <p>
+        <p> 
           CHRIST APOSTOLIC CHURCH, PA OGUN RE PRAYER CITY, also known as HOLY GHOST MINISTRY, is a mission-driven fellowship that serves as a beacon of hope and spiritual revival. 
           Rooted in faith, our church is committed to fostering a deep connection with God while creating a strong, supportive community for believers. With our general headquarters in Ibadan, Nigeria, 
           we have continued to expand across the world, touching lives and spreading the message of love, faith, and grace.
